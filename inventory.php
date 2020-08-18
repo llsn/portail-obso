c686c86b59ac71042cd31357af334c97c960de25
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-08-18 14:23:18 +0200

    mise à jour de la page inventory.php (modif 4) - retrait de l affichage de la requete SQL

--- inventory.php
+++ inventory.php
@@ -369,7 +369,7 @@ if ($table != null) {
         $count_item = $stmt->fetch();
     }
 
-    echo "<center><br><u>Requête SQL générée :</u> <br><br><textarea cols=80 rows=5 disabled>$query</textarea></center><br>";
+    /*echo "<center><br><u>Requête SQL générée :</u> <br><br><textarea cols=80 rows=5 disabled>$query</textarea></center><br>";*/
 
     echo "<center>Nombre d'occurences liées à la requête:".$count_item[0].'</center><br>'; ?>
 
