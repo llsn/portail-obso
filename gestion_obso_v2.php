23007c03cf102928a5163c53a94f9d5a96d8d1e1
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-08-19 15:30:35 +0200

    debug gestion_obso_v2.php

--- gestion_obso_v2.php
+++ gestion_obso_v2.php
@@ -331,6 +331,29 @@
 							}
 						}
 					}
+					echo "<p class=\"debug\">";
+					error_reporting(E_ALL);   // Activer le rapport d'erreurs PHP . Vous pouvez n'utiliser que cette ligne, elle donnera déjà beaucoup de détails.
+				
+					$variables = get_defined_vars(); // Donne le contenu et les valeurs de toutes les variables dans la portée actuelle
+					$var_ignore=array("GLOBALS", "_ENV", "_SERVER","_GET","host","dbname","user","password","port","socket"); // Détermine les var à ne pas afficher
+					echo ("<strong>Etat des variables a la ligne : ".__LINE__." dans le fichier : ".__FILE__."</strong><br />\n");
+					$nom_fonction=__FUNCTION__;
+					if (isset($nom_fonction)&&$nom_fonction!="")
+					{
+						echo ("<strong>Dans la fonction : ".$nom_fonction."</strong><br />\n");
+					}
+					foreach ($variables as $key=>$valeur)
+					{
+						if (!in_array($key, $var_ignore)&&strpos($key,"HTTP")===false)
+							{
+							echo "<pre class=\"debug\">";
+							echo ("$".$key." => ");
+							print_r($valeur);
+							echo "</pre>\n";
+							}
+					}
+				
+					echo "</p>";
                     ?>
 				</tr>
 				
