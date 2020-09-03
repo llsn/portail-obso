8de7afe539ea081c62542bd43f986d2c33228a13
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-03 15:32:49 +0200

    integration ibmbox.com

---
+++ ibmbox.php
@@ -0,0 +1,5 @@
+<html>
+<body>
+<iframe src="https://app.box.com/embed/s/fc6ru2etmsth3j9b2asqcs1r09ll225d?sortColumn=date&view=list" width="100%" height="100%" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
+</body>
+</html>
