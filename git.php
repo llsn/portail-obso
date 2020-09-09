dbf5ee172df761568e085211b322f4265238c0d2
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-09 10:15:28 +0200

    Création de la page de mise à jour GIT

---
+++ git.php
@@ -0,0 +1,26 @@
+<?php
+$GIT_REFRESH=isset($_POST['git_refresh']) ? $_POST['git_refresh'] : "FALSE";
+
+echo "<div>";
+echo "<form id='git_refresh' class='form-horizontal' method='POST' enctype='multipart/form-data' action='git.php'>";
+echo "<input type='hidden' name='git_refresh' value='TRUE'/>";
+echo "<input type='submit' value='lancer le rafraichissement du code avec la dernière version GIT' class='input-lg'>";
+echo "</form>";
+echo "</div>";
+
+if (isset($GIT_REFRESH) != "TRUE") 
+{
+    echo "<div><pre>";
+    exec("git pull",$output,$result);
+    print_r($output);
+    if($result=0)
+    {   
+        echo "Mise à jour réussi";
+    }
+    else
+    {
+        echo "Mise à jour échoué!!!";
+    }
+    echo "</pre></div>";
+}
+?>
