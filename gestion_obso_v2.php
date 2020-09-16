ba8e33e68da0d8318a0a61414d20965fd095bf19
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-16 11:47:06 +0200

    debug logical CI

--- gestion_obso_v2.php
+++ gestion_obso_v2.php
@@ -16,7 +16,7 @@
     // Initialisation de la connexion à la base de données
 	$con = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8',$user,$password)
         or die ('Could not connect to the database server' . pdo_connect_error());
-	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_USE_BUFFERED_QUERY);
     
 ?>
 <!-- Début de la page HTML -->
