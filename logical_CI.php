49ccac2dfea86dceb834409b5705cd72ef8efc9c
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-17 11:56:58 +0200

    init Logical_CI.php

--- logical_CI.php
+++ logical_CI.php
@@ -201,15 +201,15 @@
 				echo "<td>";
 				echo "<input  disabled>";
 				echo "</td>";
-				echo "<td>";
-				echo "<h4> Composants disponibles pour <?php echo $application; ?></h4>";
-				echo "</td>";
-
+				
 
 			}
         ?>
 		</form>
 		<?php
+			echo "<td>";
+			echo "<h4> Composants disponibles pour <?php echo $application; ?></h4>";
+			echo "</td>";
             if($application!="" && $env !="")
             {
                 echo "<td>";
