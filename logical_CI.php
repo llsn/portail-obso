d6d7cef52fe32b92e6529b2b8fcceec96767d0a0
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-17 13:14:48 +0200

    init Logical_CI.php

--- logical_CI.php
+++ logical_CI.php
@@ -416,7 +416,7 @@
 								$application=explode("#",$affichage);
 								echo "<center><H2>".$application[0]."</H2></center>";
 							}
-							$querycall="select replace(FUNCTIONALGROUPS,'|','\n'),CONFIGURATIONNAME_WO_EXTENSION,STATUS,OPERATINGENVIRONMENT, OSNAME, OSVERSION, `DB Middleware Edition`,`DB Middleware Version`,`DB Instance`, `MDW Middleware Edition`,`MDW Middleware Version` from global_inventory where functionalgroups like '%$affichage%'";
+							$querycall="select replace(FUNCTIONALGROUPS,'|','\n') as `FUNCTIONALGROUPS`,CONFIGURATIONNAME_WO_EXTENSION,STATUS,OPERATINGENVIRONMENT, OSNAME, OSVERSION, `DB Middleware Edition`,`DB Middleware Version`,`DB Instance`, `MDW Middleware Edition`,`MDW Middleware Version` from global_inventory where functionalgroups like '%$affichage%'";
 							if ($stmt = $con->prepare($querycall)) 
 							{
 								try
