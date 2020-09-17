5515adffbc19e774d31595dba4a390dcaf919c93
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-17 11:10:34 +0200

    init Logical_CI.php

--- git.php
+++ git.php
@@ -3,9 +3,9 @@ $GIT_REFRESH=isset($_POST['git_refresh']) ? $_POST['git_refresh'] : "FALSE";
 
 echo "<div>";
 echo "<form id='git_refresh' class='form-horizontal' method='POST' enctype='multipart/form-data' action='git.php'>";
-echo "<input type='hidden' name='git_refresh' value='TRUE'/>";
-echo "<input type='submit' value='lancer le rafraichissement du code avec la dernière version GIT' class='input-lg'>";
-echo "</form>";
+
+
+
 echo "</div>";
 echo "<br/><pre>".$GIT_REFRESH."</pre><br/>";
 if (isset($GIT_REFRESH) != "TRUE") 
@@ -16,12 +16,16 @@ if (isset($GIT_REFRESH) != "TRUE")
     print_r($output);
     if($result=0)
     {   
+        echo "<input type='hidden' name='git_refresh' value='TRUE'/>";
         echo "Mise à jour réussi";
     }
     else
     {
+        echo "<input type='hidden' name='git_refresh' value='FALSE'/>";
         echo "Mise à jour échoué!!!";
     }
     echo "</pre></div>";
 }
+echo "<input type='submit' value='lancer le rafraichissement du code avec la dernière version GIT' class='input-lg'>";
+echo "</form>";
 ?>
