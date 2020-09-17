6f51e9bf1c26b54f9c7e8ee77fc8720545f9d919
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-17 12:41:18 +0200

    init Logical_CI.php

--- fiche_machine.php
+++ fiche_machine.php
@@ -410,7 +410,8 @@
 			{
 				foreach ($list_functional_group as $ligne)
 				{
-					echo $ligne."<br/>\n";
+					echo "<form id='".$ligne."' method='POST' action='locigal_CI.php'><input type='hidden' name='affichage' value='".$ligne."'/><input type='hidden' name='var_consult_component' value='true'/></form><a href='#' onclick='document.getElementById(\"$ligne\").submit()' ><b>".$ligne."</b></a>";
+                    echo "<br/>";
 				}
 			}
                     	echo "</td>";
