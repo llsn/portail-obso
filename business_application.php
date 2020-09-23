122215bd8209690026a4dd3e86e2d0a9adccffff
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-23 12:52:35 +0200

    correction requete sql

--- business_application.php
+++ business_application.php
@@ -531,29 +531,29 @@
 			</center>
 		
 
-		<!-- <p class="debug"> -->
+		<p class="debug">
 		<?php
-		// 	error_reporting(E_ALL);   // Activer le rapport d'erreurs PHP . Vous pouvez n'utiliser que cette ligne, elle donnera déjà beaucoup de détails.
+			error_reporting(E_ALL);   // Activer le rapport d'erreurs PHP . Vous pouvez n'utiliser que cette ligne, elle donnera déjà beaucoup de détails.
 		
-		// 	$variables = get_defined_vars(); // Donne le contenu et les valeurs de toutes les variables dans la portée actuelle
-		// 	$var_ignore=array("GLOBALS", "_ENV", "_SERVER","_GET","host","dbname","user","password","port","socket"); // Détermine les var à ne pas afficher
-		// 	echo ("<strong>Etat des variables a la ligne : ".__LINE__." dans le fichier : ".__FILE__."</strong><br />\n");
-		// 	$nom_fonction=__FUNCTION__;
-		// 	if (isset($nom_fonction)&&$nom_fonction!="")
-		// 	{
-		// 		echo ("<strong>Dans la fonction : ".$nom_fonction."</strong><br />\n");
-		// 	}
-		// 	foreach ($variables as $key=>$valeur)
-		// 	{
-		// 		if (!in_array($key, $var_ignore)&&strpos($key,"HTTPS")===false)
-		// 			{
-		// 			echo "<pre class=\"debug\">";
-		// 			echo ("$".$key." => ");
-		// 			print_r($valeur);
-		// 			echo "</pre>\n";
-		// 			}
-		// 	}
+			$variables = get_defined_vars(); // Donne le contenu et les valeurs de toutes les variables dans la portée actuelle
+			$var_ignore=array("GLOBALS", "_ENV", "_SERVER","_GET","host","dbname","user","password","port","socket"); // Détermine les var à ne pas afficher
+			echo ("<strong>Etat des variables a la ligne : ".__LINE__." dans le fichier : ".__FILE__."</strong><br />\n");
+			$nom_fonction=__FUNCTION__;
+			if (isset($nom_fonction)&&$nom_fonction!="")
+			{
+				echo ("<strong>Dans la fonction : ".$nom_fonction."</strong><br />\n");
+			}
+			foreach ($variables as $key=>$valeur)
+			{
+				if (!in_array($key, $var_ignore)&&strpos($key,"HTTPS")===false)
+					{
+					echo "<pre class=\"debug\">";
+					echo ("$".$key." => ");
+					print_r($valeur);
+					echo "</pre>\n";
+					}
+			}
 		?>
-		<!-- </p>  -->
+		</p> 
 </body>
 </html>
