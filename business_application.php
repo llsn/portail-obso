64130b31494f81f305b5d8938a21cfe55e89fc66
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-23 12:34:27 +0200

    generation des liens vers logical_CI.php

--- business_application.php
+++ business_application.php
@@ -480,8 +480,7 @@
 														{
 															echo "<form id='".$component."' method='POST' action='logical_CI.php'><input type='hidden' name='affichage' value='".$component."'/><input type='hidden' name='var_consult_component' value='true'/></form><a href='#' onclick='document.getElementById(\"$component\").submit()' ><b>".$component."</b></a><br/>";
                                                         }
-                                                        echo "<td/>";
-														// echo "<form id='".$col."' method='POST' action='logical_CI.php'><input type='hidden' name='affichage' value='".$col."'/><input type='hidden' name='var_consult_component' value='true'/></form><a href='#' onclick='document.getElementById(\"$col\").submit()' ><b>".$col."</b></a><br/>";
+                                                        echo "</td>";
 													break;
                                                 }
                                                 
