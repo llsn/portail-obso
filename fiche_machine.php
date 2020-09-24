61cd6092ac46ff06f6cbbd198907eb0b496e31e4
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-24 11:39:02 +0200

    formatage fiche_machine.php

--- fiche_machine.php
+++ fiche_machine.php
@@ -307,7 +307,7 @@
                     {
                         case (stristr($osname,'aix')):
                             echo "<th colspan='1'>Fils de</th>";
-                            echo "<td colspan='2'><form id='".$parent."' method='POST' action='fiche_machine.php'><input type='hidden' name='machine' value='".$parent."'/></form><a href='#' onclick='document.getElementById(\"$parent\").submit()' ><b>".$parent."</b></a><td>";
+                            echo "<td colspan='1'><form id='".$parent."' method='POST' action='fiche_machine.php'><input type='hidden' name='machine' value='".$parent."'/></form><a href='#' onclick='document.getElementById(\"$parent\").submit()' ><b>".$parent."</b></a><td>";
                         break;
                         case (stristr($osname,'windows')):
                             $subwinquery="select distinct `parent` from cmdb.relationship where `parent`= '".$parent."';";
