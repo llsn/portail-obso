3ed3570f582a2876ebbc0ab04f397867fcb0b0a9
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-24 12:14:21 +0200

    ajout de la fonction commentaire dans gestion_obso_v2.php

--- gestion_obso_v2.php
+++ gestion_obso_v2.php
@@ -2,6 +2,7 @@
 	//chargements de variable globale et des functions
 	require "variable-db.php";
 	require "functions.php";
+	require_once("DBController.php");
 
 	//Récupération des variables transmises par d'autres pages et intialisation des variables
     $application=isset($_POST['application']) ? $_POST['application'] : NULL;
@@ -19,7 +20,11 @@
 	$pdo = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8',$user,$password)
         or die ('Could not connect to the database server' . pdo_connect_error());
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+	$pdo->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+	
+
+	$db_handle = new DBController();
+	$comments = $db_handle->runQuery("SELECT * FROM comment");
 ?>
 <!-- Début de la page HTML -->
 <html>
@@ -67,7 +72,7 @@
 		<script type="text/javascript" language="javascript" src="Datatables/Responsive-2.2.3/js/responsive.jqueryui.min.js"></script>
 		<script type="text/javascript" language="javascript" src="Datatables/default/datatables.js"></script>
 
-
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
 		<!-- Initialisation des tableaux de données de la page -->
 		<script>
 			
@@ -128,7 +133,60 @@
 				document.getElementById('chargement').style.display = 'none';
 				document.getElementById('site').style.visibility = 'visible';
 			}
-
+			
+			function showEditBox(editobj,id) 
+			{
+				$('#frmAdd').hide();
+				$(editobj).prop('disabled','true');
+				var currentMessage = $("#message_" + id + " .message-content").html();
+				var editMarkUp = '<textarea rows="5" cols="80" id="txtmessage_'+id+'">'+currentMessage+'</textarea><button name="ok" onClick="callCrudAction(\'edit\','+id+')">Save</button><button name="cancel" onClick="cancelEdit(\''+currentMessage+'\','+id+')">Cancel</button>';
+				$("#message_" + id + " .message-content").html(editMarkUp);
+			}
+			function cancelEdit(message,id) 
+			{
+				$("#message_" + id + " .message-content").html(message);
+				$('#frmAdd').show();
+			}
+			function callCrudAction(action,id) 
+			{
+				$("#loaderIcon").show();
+				var queryString;
+				switch(action) 
+				{
+					case "add":
+						queryString = 'action='+action+'&txtmessage='+ $("#txtmessage").val();
+					break;
+					case "edit":
+						queryString = 'action='+action+'&message_id='+ id + '&txtmessage='+ $("#txtmessage_"+id).val();
+					break;
+					case "delete":
+						queryString = 'action='+action+'&message_id='+ id;
+					break;
+				}	 
+				jQuery.ajax({
+				url: "crud_action.php",
+				data:queryString,
+				type: "POST",
+				success:function(data){
+					switch(action) {
+						case "add":
+							$("#comment-list-box").append(data);
+						break;
+						case "edit":
+							$("#message_" + id + " .message-content").html(data);
+							$('#frmAdd').show();
+							$("#message_"+id+" .btnEditAction").prop('disabled','');	
+						break;
+						case "delete":
+							$('#message_'+id).fadeOut();
+						break;
+					}
+					$("#txtmessage").val('');
+					$("#loaderIcon").hide();
+				},
+				error:function (){}
+				});
+			}
 			
 	
 		</script>
@@ -193,6 +251,12 @@
 
 				pointer-events: none;
 			}
+			
+			body{width:40%;}
+			.message-box{margin-bottom:20px;border-top:#F0F0F0 2px solid;background:#FAF8F8;padding:10px;}
+			.btnEditAction{background-color:#2FC332;border:0;padding:2px 10px;color:#FFF;}
+			.btnDeleteAction{background-color:#D60202;border:0;padding:2px 10px;color:#FFF;margin-bottom:15px;}
+			#btnAddAction{background-color:#09F;border:0;padding:5px 10px;color:#FFF;}
 		</style>
 		<script>
 			window.paceOptions = {
@@ -366,6 +430,7 @@
 				<li><a data-toggle="tab" href="#db">Base de données</a></li>
 				<li><a data-toggle="tab" href="#mdw">Middleware</a></li>
 				<li><a data-toggle="tab" href="#LCI">Logical CI</a></li>
+				<li><a data-toggle="tab" href="#notes">Notes</a></li>
 				<li><a data-toggle="tab" href="#debug">Debug</a></li>
 			</ul>
 			<!-- définition de la zone de contenu d'onglet -->
@@ -801,6 +866,31 @@
 				<?php
 				$PDF_SHEET = ob_get_contents();	
 				?>
+				<div id="notes" class="tab-pane fade">
+				<div class="form_style">
+					<div id="comment-list-box">
+					<?php
+					if(!empty($comments)) {
+					foreach($comments as $k=>$v) {
+					?>
+					<div class="message-box" id="message_<?php echo $comments[$k]["id"];?>">
+					<div>
+					<button class="btnEditAction" name="edit" onClick="showEditBox(this,<?php echo $comments[$k]["id"]; ?>)">Edit</button>
+					<button class="btnDeleteAction" name="delete" onClick="callCrudAction('delete',<?php echo $comments[$k]["id"]; ?>)">Delete</button>
+					</div>
+					<div class="message-content"><?php echo $comments[$k]["message"]; ?></div>
+					</div>
+					<?php
+					}
+					} ?>
+					</div>
+
+					<div id="frmAdd"><textarea name="txtmessage" id="txtmessage" cols="80" rows="5"></textarea>
+					<p><button id="btnAddAction" name="submit" onClick="callCrudAction('add','')">Add</button></p>
+					</div>
+					<img src="LoaderIcon.gif" id="loaderIcon" style="display:none" />
+					</div>
+				</div>
 				<div id="debug" class="tab-pane fade">
 					<p class="debug">
 					<?php
