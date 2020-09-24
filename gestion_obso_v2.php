9e5aa7556f9f33f27583605a2021653653cf9b7b
Lionel SEVERIAN <ext.lseverian@cma-cgm.com>
2020-09-24 12:35:22 +0200

    ajout de la fonction commentaire dans gestion_obso_v2.php

--- gestion_obso_v2.php
+++ gestion_obso_v2.php
@@ -251,8 +251,8 @@
 
 				pointer-events: none;
 			}
-			
-			body{width:40%;}
+
+			/*body{width:40%;}*/
 			.message-box{margin-bottom:20px;border-top:#F0F0F0 2px solid;background:#FAF8F8;padding:10px;}
 			.btnEditAction{background-color:#2FC332;border:0;padding:2px 10px;color:#FFF;}
 			.btnDeleteAction{background-color:#D60202;border:0;padding:2px 10px;color:#FFF;margin-bottom:15px;}
@@ -869,24 +869,30 @@
 				<div id="notes" class="tab-pane fade">
 				<div class="form_style">
 					<div id="comment-list-box">
-					<?php
-					if(!empty($comments)) {
-					foreach($comments as $k=>$v) {
-					?>
-					<div class="message-box" id="message_<?php echo $comments[$k]["id"];?>">
-					<div>
-					<button class="btnEditAction" name="edit" onClick="showEditBox(this,<?php echo $comments[$k]["id"]; ?>)">Edit</button>
-					<button class="btnDeleteAction" name="delete" onClick="callCrudAction('delete',<?php echo $comments[$k]["id"]; ?>)">Delete</button>
-					</div>
-					<div class="message-content"><?php echo $comments[$k]["message"]; ?></div>
-					</div>
-					<?php
-					}
-					} ?>
+						<?php
+							if(!empty($comments)) 
+							{
+								foreach($comments as $k=>$v) 
+								{
+									?>
+									<div class="message-box" id="message_<?php echo $comments[$k]["id"];?>">
+										<div>
+											<button class="btnEditAction" name="edit" onClick="showEditBox(this,<?php echo $comments[$k]["id"]; ?>)">Editer</button>
+											<button class="btnDeleteAction" name="delete" onClick="callCrudAction('delete',<?php echo $comments[$k]["id"]; ?>)">Supprimer</button>
+										</div>
+									<div class="message-content">
+										<?php echo $comments[$k]["message"]; ?></div>
+									</div>
+									<?php
+								}
+							} 
+						?>
 					</div>
 
-					<div id="frmAdd"><textarea name="txtmessage" id="txtmessage" cols="80" rows="5"></textarea>
-					<p><button id="btnAddAction" name="submit" onClick="callCrudAction('add','')">Add</button></p>
+					<div id="frmAdd">
+						<textarea name="txtmessage" id="txtmessage" cols="80" rows="5"></textarea>
+						<p>
+						<button id="btnAddAction" name="submit" onClick="callCrudAction('add','')">Ajouter</button></p>
 					</div>
 					<img src="LoaderIcon.gif" id="loaderIcon" style="display:none" />
 					</div>
